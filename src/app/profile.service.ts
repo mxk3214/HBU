a7e2547fe3e3ac6f63db91986ebe92e64f0731ab
brandonmok <brandonmok88@yahoo.com>
2019-04-13 11:28:33 -0400

    Added starts of profile service

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { Profile } from './profile';
+import { HttpClient} from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
+  // Array of profiles
+  profiles: Profile[] = [];
+  httpUrl: string = 'https://reqres.in/api/users';
+  
+  // Constructor
+  constructor(private http: HttpClient) { 
+    this.profiles = this.http.get(this.httpUrl);
+  }
 
-  constructor() { }
+  // Get Profiles Method
+  getProfiles(): Observable<Profile[]>{
+    return this.http.get<any>(this.httpUrl).pipe(
+        catchError((err) => {
+          console.error(err);
+          return of({data: []});
+        }), 
+        map( res => res.data as Profile[])
+    );
+  }
 }
---
+++ src/app/profile.ts
@@ -0,0 +1,6 @@
+export class Profile {
+    id: number;
+    first_name: string;
+    last_name: string;
+    avatar: string;
+}
