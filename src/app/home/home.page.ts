773430ae1adb2002952ed2276646874df5eb72d3
Mykhaylo Kachaluba <misha819k@gmail.com>
2019-04-28 14:41:52 -0400

    Here are my suggested changes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
-import { OnInit } from "@angular/core";
+import { OnInit } from '@angular/core';
 
 import { ImageService } from '../image.service';
 import { Image } from '../image';
@@ -11,39 +11,25 @@ import { Image } from '../image';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit{
-  visible: boolean = true;
+export class HomePage implements OnInit {
+  visible: Boolean = true;
 
   images: Image[] = [];
-  
-  imageStore: Observable<Image>[] = [];
-  
 
-  constructor(private imageService: ImageService){}
+  imageStore: Observable<Image[]>;
+
+  constructor(private imageService: ImageService) { }
 
   ngOnInit() {
     this.getAllImages();
-  } 
-
-  async getAllImages() {
-    // Initial way that worked with only an observable of images
-    // const obs: Observable<Image[]>  = await this.imageService.getImages();
-    // obs.subscribe(
-    //   (imgs: Image[]) => this.images = imgs
-    // );
-
-    // Attempted new way
-    // NOW an Observable array of images
-    const obs: Observable<Image>[]  = await this.imageService.getImages(); 
-    // Won't work bc need to subscribe the the array of observables to get the images
-    obs.subscribe( 
-        (imgs: Observable<Image>[]) => this.imageStore = imgs
-    );
   }
 
-  
+  getAllImages() {
+    this.imageStore = this.imageService.getImages();
+  }
+
   // Might remove...
-  toggleHeart(){
+  toggleHeart() {
     this.visible = !this.visible;
   }
 }
--- src/app/image.service.ts
+++ src/app/image.service.ts
@@ -9,29 +9,25 @@ import { Image } from './image';
   providedIn: 'root'
 })
 export class ImageService {
-  images: Image[] = []; 
-  
-  // URL to API
-  httpUrl: string = "https://picsum.photos/v2/list?page=1&limit=6";
+  images: Image[] = [];
 
-  constructor(private http : HttpClient) {}
+  // URL to API
+  httpUrl = 'https://picsum.photos/v2/list?page=1&limit=6';
+
+  constructor(private http: HttpClient) { }
 
   // GetImages from API
-  getImages() {
-    return this.http.get<any>(this.httpUrl).pipe(
+  getImages(): Observable<Image[]> {
+    return this.http.get<Image[]>(this.httpUrl).pipe(
       catchError((err) => {
         console.error(err);
-        return of({data: []});
+        return of([]);
       }),
+      map( res => res as Image[])
+    );
+  }
 
-      //map( res => res as Image[])  //OLD Way
-
-      // NEW way of observable array of images
-      map( res => res as Observable<Image>[])
-    )
-  }  
-
-  addImage(newImage: Image){
+  addImage(newImage: Image) {
     this.images.push(newImage);
   }
 }
